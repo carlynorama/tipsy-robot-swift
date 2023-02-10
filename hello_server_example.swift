bbbc0d5c613bb3e2c2b8a42356eaec9c90a6c2f7
carlynorama <carlynorama@gmail.com>
2023-02-09 18:52:20 -0800

    Create hello_server_example.swift

---
+++ hello_server_example.swift
@@ -0,0 +1,37 @@
+import Foundation
+import ArgumentParser
+import TrunkLine
+
+
+public private(set) var botServer:MastodonServer = makeServer()
+
+func makeServer() -> MastodonServer {
+    do {
+        try TrunkLine.loadDotEnv()
+    } catch {
+        fatalError("Couldn't load .env")
+    }
+    let url = URL(string: ProcessInfo.processInfo.environment["SERVER_NAME"]!)
+    var newServer:MastodonServer = MastodonServer(host: url!)
+    do { 
+    try newServer.authorize(try TrunkLine.fetchAuthFromEnvironment(
+            account: "my_bot_name",                  //these aren't important since only one account
+            service: "masto_bot",                    //and we aren't saving to the keychain. 
+            tokenKey: "SERVER_TOKEN"))               //this is important, must match .env
+    } catch { 
+        print(error) 
+    }
+     return newServer
+}
+
+@main
+public struct your_bot_project_name {
+
+    public static func main() async throws {
+
+       try await botServer.writeSimpleStatus(string: "Hello, world.")
+        // let data = try await botServer.checkCredential(authentication:botServer.authentication!)
+        // print("data: \(String(data: data, encoding: .utf8))")
+       
+    }
+}
