c0f476837bb87f21ddc57e95a6af071629ce2a0f
carlynorama <carlynorama@gmail.com>
2023-02-25 15:24:33 -0800

    make a timer

--- Sources/tipsy-robot-swift/tipsy_robot_swift.swift
+++ Sources/tipsy-robot-swift/tipsy_robot_swift.swift
@@ -31,19 +31,40 @@ public struct tipsy_robot_swift {
        //Check credential will pass if credential is in the .env file as shown.
        //it is loaded into the server during the "makeServer" function. 
        let response =  try await botServer.checkCredential()
-       print(String(data:response, encoding: .utf8)!)
+       //print(String(data:response, encoding: .utf8)!)
 
-       
-        let message = "Checking in on the CLI after the big refactor... \(Date.now)..."
+       let timer = testTimer()
+       // let message = "Checking in on the CLI after the big refactor... \(Date.now)..."
        
        // simplest message post.
        //try await botServer.newPost(message: message)
 
        //simplest image post NOTE: This function name will change. 
-       let imagePostMessage = "Posting and image from the CLI... \(Date.now)."
-       let imageFile = "small_jpg_test.jpg"
-       let imageAltText = "This image again? Flowers, woodland floor, blah blah blah."
-       try await botServer.newPostWithImage(message: imagePostMessage, imageFilePath: imageFile , imageAltText: imageAltText)
+    //    let imagePostMessage = "Posting an image from the CLI... \(Date.now)."
+    //    let imageFile = "small_jpg_test.jpg"
+    //    let imageAltText = "This image again? Flowers, woodland floor, blah blah blah."
+    //    try await botServer.newPostWithImage(message: imagePostMessage, imageFilePath: imageFile , imageAltText: imageAltText)
        
     }
+
+
+
 }
+
+    func testTimer() -> Timer {
+        let timer = Timer.scheduledTimer(withTimeInterval: 30.0, repeats: true) { timer in
+            Task { 
+            let message = "Posting on a quick timer... \(Date.now)..."
+            try await botServer.newPost(message: message)
+           }
+        }
+        timer.tolerance = 1.0 //give it some latitude to timer coalesce
+
+        withExtendedLifetime(timer) {
+            RunLoop.current.run()
+            //RunLoop.current.add(timer, forMode: .common)
+        }
+        return timer
+    }
+
+
